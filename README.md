82f3599f1549f87bea51d1bb34bd40d79627f1f0
Martijn Visser <mgvisser@gmail.com>
2018-03-23 16:24:20 +0100

    Add autogenerated files to build GEOS

---
+++ README.md
@@ -0,0 +1,9 @@
+# GEOSBuilder
+
+[![Build Status](https://travis-ci.org/visr/GEOSBuilder.svg?branch=master)](https://travis-ci.org/visr/GEOSBuilder)
+
+This repository builds binary artifacts for the GEOS project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/visr/GEOSBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,44 @@
+using BinaryBuilder
+
+# Collection of sources required to build GEOS
+sources = [
+    "http://download.osgeo.org/geos/geos-3.6.2.tar.bz2" =>
+    "045a13df84d605a866602f6020fc6cbf8bf4c42fb50de237a08926e1d7d7652a",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd geos-3.6.2/
+./configure --prefix=$prefix --host=$target
+make -j2
+make install
+
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    BinaryProvider.Linux(:i686, :glibc),
+    BinaryProvider.Linux(:x86_64, :glibc),
+    BinaryProvider.Linux(:aarch64, :glibc),
+    BinaryProvider.Linux(:armv7l, :glibc),
+    BinaryProvider.Linux(:powerpc64le, :glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libgeos_c", :libgeos_c),
+    LibraryProduct(prefix, "libgeos", :libgeos)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "GEOS", sources, script, platforms, products, dependencies)
+
