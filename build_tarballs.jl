9d0705dec5829c7a73c0b82f53af9db38d6b6f1d
Martijn Visser <mgvisser@gmail.com>
2018-10-17 13:51:02 +0200

    build GEOS 3.7.0 and add all supported platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,17 +1,17 @@
 using BinaryBuilder
 
-src_version = v"3.6.2"  # also change in raw script string
+src_version = v"3.7.0"  # also change in raw script string
 
 # Collection of sources required to build GEOS
 sources = [
     "http://download.osgeo.org/geos/geos-$src_version.tar.bz2" =>
-    "045a13df84d605a866602f6020fc6cbf8bf4c42fb50de237a08926e1d7d7652a",
+    "4fbf41a792fd74293ab59e0a980e8654cd411a9d45416d66eaa12d53d1393fd7",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd geos-3.6.2/
+cd geos-3.7.0/
 ./configure --prefix=$prefix --host=$target
 make -j${nproc}
 make install
@@ -19,16 +19,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS(),
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -37,10 +28,7 @@ products(prefix) = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-    
-]
+dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, "GEOS", src_version, sources, script, platforms, products, dependencies)
-
